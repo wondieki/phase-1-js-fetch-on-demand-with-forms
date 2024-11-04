174dc29a0e0571f227ef169b3eb4284f6d45ff9e
wondieki <wkerubo28@gmail.com>
2024-11-04 10:51:52 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const search = document.getElementById("searchByID").value;
+      // console.log(event.target.children[1].value);
+      // console.log(search.value)
+      fetch(`http://localhost:3000/movies/${search}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+          title.innerText = data.title;
+          summary.textContent = data.summary;
+        });
+    });
+  };
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+  document.addEventListener("DOMContentLoaded", init);
+  fetch("http://localhost:3000/movies", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: "Avengers Endgame",
+      summary:
+        "Earth heroes unite to bring back people who were wipped away by the mad titan",
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
